a665d7345e3b2740075f072971c2510032f2d979
banned-historical-archives <109972625+banned-historical-archives@users.noreply.github.com>
2024-01-02 23:01:44 +0800

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -7,5 +7,6 @@ from PIL import Image, ImageDraw, ImageFont
 import paddleocr.tools.infer.utility as utility
 from utils import draw_ocr_results, start_ocr
 
-args = json.loads(sys.argv[1])
-print(start_ocr(args))
+with open(sys.argv[1]) as f:
+  args = json.load(f)
+  print(start_ocr(args))
